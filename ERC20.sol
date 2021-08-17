946fe1d25f17728505270ac83960a5a8fdea2b53
gmsolem <geir.solem.m@gmail.com>
2021-08-17 11:20:28 -0500

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -11,7 +11,7 @@ pragma solidity ^0.5.0;
 // WBIB is an ERC-20 token that’s hold a one-to-one peg to the value of BIB 
 // allowing you to effectively use BIB on ERC20 based Ethereum network/exchanges.
 // ----------------------------------------------------------------------------
-contract ERC20Interface {
+contract WrappedBIB {
     function totalSupply() public view returns (uint);
     function balanceOf(address tokenOwner) public view returns (uint balance);
     function allowance(address tokenOwner, address spender) public view returns (uint remaining);
