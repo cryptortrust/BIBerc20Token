c57dde2449a2bd24a1427da46915000eb23fbb65
gmsolem <geir.solem.m@gmail.com>
2021-08-17 11:07:44 -0500

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -3,7 +3,7 @@
 */
 
 // SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.4;
+pragma solidity ^0.5.0;
 
 // ----------------------------------------------------------------------------
 // ERC Token Standard #20 Interface
